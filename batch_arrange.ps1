393f874ed730f463eb4a1a9108d1483317a70465
Chris Duncombe Rae <christopher.duncombe.rae@noaa.gov>
2014-08-21 13:52:23 -0400

    Forgot to save changes before commiting.

--- batch_arrange.ps1
+++ batch_arrange.ps1
@@ -1,19 +1,22 @@
 # power shell script to remove contents of .\ except output\ and then move contents of output\ up into .\
 
+# run this script after pulling the source (master) from github, making changes, pushing them to github, 
+# running pelican contents to populate output\, and changing the branch to gh-pages.
+# The script will transfer output up to gh-pages. Then push the branch (excluding output) up to github, and yoour 
+# new pages appear on the website at github.io
+
 # a list of files in .\, not including output\
-
 # $files=get-childitem -name | grep -v "output"
-
 # more better:
-$files=get-childitem -name -Exclude "output"
+$files=get-childitem -name -Exclude "output",".git*","batch_arrange.ps1" 
+
 # $files
 
-$newfiles=get-childitem -name -Path ".\output\"
-# $newfiles
+# nuke $files by passing the list to Remove-Item, being sure to Recurse, else complaints ensue
+$files | Remove-Item -Recurse
 
-# woo-woo!
+# and move the contents of output to the current (hoping for no overlap)
+Move-Item output\* .
 
-# nuke $files 
-
-
-# and move $newfiles
+# now we should be able to 
+#      git push 
